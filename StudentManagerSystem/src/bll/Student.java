a56bd7fa2dce1dd378eba9ecda6f14bc33377db5
bapie-panchen <1436804089@qq.com>
2022-11-09 17:07:29 +0800

    第一次提交

---
+++ StudentManagerSystem/src/bll/Student.java
@@ -0,0 +1,40 @@
+package bll;
+
+public class Student {
+    private String id;
+    private String num;
+    private String name;
+    private float score;
+
+    public Student() {
+    }
+    public Student(String id, String num, String name, float score) {
+        this.id = id;
+        this.num = num;
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+    public String getNum() {return this.num;}
+    public String getName() {
+        return this.name;
+    }
+    public float getScore() {
+        return this.score;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setNum(String num) {
+        this.num = num;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setScore(float score) {
+        this.score = score;
+    }
+}
---
+++ StudentManagerSystem/src/ui/Test.java
@@ -0,0 +1,12 @@
+package ui;
+
+import bll.Student;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Student[] stu = new Student[10];
+
+    }
+}
